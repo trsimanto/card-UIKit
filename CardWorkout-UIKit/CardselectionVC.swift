0c0290ba5ec3530010ea26bec90b6eb6243dc379
trsimanto <simanto14feb@gmail.com>
2023-01-08 09:12:02 +0600

    ongoing

--- CardWorkout-UIKit/CardselectionVC.swift
+++ CardWorkout-UIKit/CardselectionVC.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class CardselectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var stopButton: UIButton!
+    @IBOutlet var restartButton: UIButton!
+    @IBOutlet var rulesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        stopButton.layer.cornerRadius = 8
+        restartButton.layer.cornerRadius = 8
+        rulesButton.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        stopButton.setTitle("I have tapped", for: .normal)
+    }
+    
 
 }
